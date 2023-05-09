0341f7685b2c7f1c89125bb595b5cc120db23c90
Jonathan Valdes <jonathanvaldes@Jonathans-Air.hitronhub.home>
2023-05-09 10:01:38 -0400

    frontend tables

--- client/pages/Home.jsx
+++ client/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Box,
   Card,
@@ -7,8 +7,10 @@ import {
   Toolbar,
   Paper,
   Typography,
+  Button,
 } from '@mui/material';
-
+import AddIcon from '@mui/icons-material/Add';
+import AddModal from '../components/AddModal.jsx';
 const styles = {
   root: {
     flexGrow: 1,
@@ -23,35 +25,108 @@ const styles = {
     textAlign: 'left',
   },
   column: {
-    height: '100vh',
+    height: 'calc(98vh - 64px)',
     border: '1px solid #ccc',
-    padding: '20px',
     boxSizing: 'border-box',
+  },
+  container: {
+    marginTop: '58px',
+    width: '90%',
+  },
+  card: {
+    width: '100%',
+    height: '90px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
   },
 };
 
 const Home = () => {
+  const [open, setOpen] = useState(false);
+  const [option, setOption] = useState('');
   return (
     <Box sx={styles.root}>
       <AppBar position='fixed' sx={styles.appBar}>
         <Toolbar sx={styles.title}>Welcome!</Toolbar>
       </AppBar>
-      <Grid container spacing={0} sx={{ width: '95%' }}>
-        <Grid item xs={3} sx={styles.column}>
-          <Box>
-            <Typography>Hello</Typography>
-          </Box>
+      <Box sx={styles.container}>
+        <Grid container spacing={0}>
+          <Grid item xs={3} sx={styles.column}>
+            <Card sx={styles.card}>
+              <Typography
+                variant='h5'
+                sx={{ textAlign: 'center', p: '10px', pb: '5px' }}
+              >
+                Applied
+              </Typography>
+              <Button
+                onClick={() => {
+                  setOption('applied');
+                  setOpen(true);
+                }}
+              >
+                <AddIcon />
+              </Button>
+            </Card>
+          </Grid>
+          <Grid item xs={3} sx={styles.column}>
+            <Card sx={styles.card}>
+              <Typography
+                variant='h5'
+                sx={{ textAlign: 'center', p: '10px', pb: '5px' }}
+              >
+                Interview
+              </Typography>
+              <Button
+                onClick={() => {
+                  setOption('interview');
+                  setOpen(true);
+                }}
+              >
+                <AddIcon />
+              </Button>
+            </Card>
+          </Grid>
+          <Grid item xs={3} sx={styles.column}>
+            <Card sx={styles.card}>
+              <Typography
+                variant='h5'
+                sx={{ textAlign: 'center', p: '10px', pb: '5px' }}
+              >
+                Offer
+              </Typography>
+              <Button
+                onClick={() => {
+                  setOption('offer');
+                  setOpen(true);
+                }}
+              >
+                <AddIcon />
+              </Button>
+            </Card>
+          </Grid>
+          <Grid item xs={3} sx={styles.column}>
+            <Card sx={styles.card}>
+              <Typography
+                variant='h5'
+                sx={{ textAlign: 'center', p: '10px', pb: '5px' }}
+              >
+                Rejected
+              </Typography>
+              <Button
+                onClick={() => {
+                  setOption('rejected');
+                  setOpen(true);
+                }}
+              >
+                <AddIcon />
+              </Button>
+            </Card>
+          </Grid>
         </Grid>
-        <Grid item xs={3} sx={styles.column}>
-          2
-        </Grid>
-        <Grid item xs={3} sx={styles.column}>
-          3
-        </Grid>
-        <Grid item xs={3} sx={styles.column}>
-          4
-        </Grid>
-      </Grid>
+      </Box>
+      <AddModal open={open} setOpen={setOpen} option={option} setOption={setOption}/>
     </Box>
   );
 };
