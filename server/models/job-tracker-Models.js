c13c19d9c2bfe2edfd614abcf2ca8e945b6003c6
Slava Melikov <samuslav@gmail.com>
2023-05-09 12:02:13 -0400

    databases

--- server/models/job-tracker-Models.js
+++ server/models/job-tracker-Models.js
@@ -25,6 +25,29 @@ pool
     console.error('Error creating table users: ', err);
   });
 
+pool
+  .query(
+    `
+  CREATE TABLE IF NOT EXISTS jobs (
+    id SERIAL PRIMARY KEY,
+    company_name VARCHAR(255),
+    job_title VARCHAR(255),
+    interest_level VARCHAR(255),
+    status VARCHAR(255),
+    url VARCHAR(255),
+    date VARCHAR(255),
+    description VARCHAR(255),
+    users_id INTEGER REFERENCES users(id)
+  )
+`
+  )
+  .then(() => {
+    console.log('Table jobs created successfully');
+  })
+  .catch((err) => {
+    console.error('Error in creating table jobs: ', err);
+  });
+
 module.exports = {
   query: (text, params, callback) => {
     console.log('executed query', text);
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const userRouter = require('./routes/userRoutes');
+const jobRouter = require('./routes/jobRoutes');
 
 const app = express();
 app.use(express.json());
@@ -10,7 +11,7 @@ app.use(express.static(path.join(__dirname, './../build')));
 
 //routes
 app.use('/user', userRouter);
-
+app.use('/job', jobRouter);
 //allows refresh and direct navigation for react router
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, '../build/index.html'), function (err) {
